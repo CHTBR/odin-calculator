b2cb571bcd83a8c185a3ed09440295744c65355b
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-06-19 10:08:52 +0200

    Add script file and abstracted display update

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonCOntainer = document.getElementById("button-container");
+const display = document.getElementById("display");
+
+function updateDisplay(text) {
+    display.textContent = text;
+}
