2ad2d577b3fb3a9920880bdb39e904fb3f9ab2ba
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-06-19 10:17:37 +0200

    Add script for inputing numbers of up to 6 digits

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const buttonCOntainer = document.getElementById("button-container");
 const display = document.getElementById("display");
+let firstNumber = "";
+let operation = "";
+let secondNumber = "";
+let result ="";
+
+buttonCOntainer.addEventListener("click", event =>{
+    const target = event.target;
+    if (target.tagName === "BUTTON") {
+        console.log("recognised as button")
+        if (target.classList.item(0) === "number-button") {
+            console.log("recognised as number button")
+            if (!operation) {
+                if (firstNumber.length < 6) {
+                    firstNumber += target.id;
+                    updateDisplay(firstNumber);
+                }
+            } else {
+                if (secondNumber.length < 6) {
+                    secondNumber += target.id;
+                    updateDisplay(secondNumber);
+                }
+            }
+        }
+    }
+});
 
 function updateDisplay(text) {
     display.textContent = text;
