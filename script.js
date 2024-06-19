f40e19162bce8b2172a501444a7d6a08e8467772
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-06-19 13:54:51 +0200

    Add script for evaluating and rounding to 6 digits

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const buttonCOntainer = document.getElementById("button-container");
+const buttonContainer = document.getElementById("button-container");
 const display = document.getElementById("display");
 let firstNumber = "";
 let operation = "";
 let secondNumber = "";
 let result ="";
 
-buttonCOntainer.addEventListener("click", event =>{
+buttonContainer.addEventListener("click", event =>{
     const target = event.target;
     if (target.tagName === "BUTTON") {
         console.log("recognised as button")
@@ -28,4 +28,29 @@ buttonCOntainer.addEventListener("click", event =>{
 
 function updateDisplay(text) {
     display.textContent = text;
+}
+
+function returnEvaluatedExpression(firstNumber, secondNumber, operation) {
+    firstNumber = +firstNumber;
+    secondNumber = +secondNumber;
+    switch (operation) {
+        case "add":
+            return String(firstNumber + secondNumber);
+        case "substract":
+            return String(firstNumber - secondNumber);
+        case "multiply":
+            return String(returnRoundedNumber(firstNumber * secondNumber));
+        case "divide":
+            return String(returnRoundedNumber(firstNumber / secondNumber));
+    }
+}
+
+function returnRoundedNumber(number) {
+    if (number > 1 || number < -1) {
+        return String(number.toPrecision(6));
+    } else {
+        longNumber = (number).toExponential();
+        shortNumber = longNumber.match(/[\d.]{6}/) + longNumber.match(/e-\d+/);
+        return shortNumber;
+    }
 }
